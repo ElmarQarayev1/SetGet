dfba14b3e9b9fc457f3964443251d2c863114707
elmar <elmar@elmar-Air.bbrouter>
2024-01-17 00:49:01 +0400

    tapsiriq yerine yetirildi

---
+++ SetGetTask1/SetGetTask1/ChinaCity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SetGetTask;
+
+namespace SetGetTask
+{
+    public class ChinaCity : City
+    {
+
+        public override int Population
+        {
+            get
+            {
+                return _population;
+            }
+            set
+            {
+                if (value >= 100000)
+                {
+                    _population = value;
+                }
+            }
+        }
+    }
+}
+
---
+++ SetGetTask1/SetGetTask1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace SetGetTask;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string Strsay;
+        int say;
+        do
+        {
+            Console.WriteLine("nece eded seher yerlesdireceksiniz?");
+            Strsay = Console.ReadLine();
+
+        } while (!int.TryParse(Strsay, out say));
+
+        ChinaCity[] chinaCities = new ChinaCity[say];
+
+        for (int i = 0; i < say; i++)
+        {
+            ChinaCity chinaCity = new ChinaCity();
+            do
+            {
+                Console.WriteLine("seherin adini daxil edin:");
+                chinaCity.Name = Console.ReadLine();
+            } while (String.IsNullOrWhiteSpace(chinaCity.Name));
+
+            string StrchinaCityPop;
+            int chinaCityPop;
+            do
+            {
+                Console.WriteLine("seherin ehalisini daxil edin:");
+                StrchinaCityPop = Console.ReadLine();
+
+            } while (!int.TryParse(StrchinaCityPop, out chinaCityPop));
+
+            chinaCity.Population = chinaCityPop;
+
+            chinaCities[i] = chinaCity;
+        }
+        var avaragePopulation = 0;
+        for (int i = 0; i < chinaCities.Length; i++)
+        {
+            Console.WriteLine($"{chinaCities[i].Name}-{chinaCities[i].Population}");
+            avaragePopulation += chinaCities[i].Population;
+        }
+        Console.WriteLine($"ortalama ehali: " + avaragePopulation / chinaCities.Length);
+
+        Console.ReadLine();
+    }
+}
+
